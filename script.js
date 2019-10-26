04b8fd3184865ac6632f6952a1727bb826de471e
OmarBriceno09 <ec2-user@ip-172-31-39-13.us-east-2.compute.internal>
2019-10-26 23:53:23 +0000

    finalized vue project

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ let app = new Vue({
     data:{
         key: '2161b383a811f994f0a98f1d5e0d67fe32b7b15d',
         country: '&country=US',
-        year: '&year=2019',
-        month: '&month=12',
-        day: '&day=24',
-        curr_year: '',
-        curr_month: '',
-        curr_day: '',
+        year: '',
+        month: '',
+        day: '',
+        curr_year: 0,
+        curr_month: 0,
+        curr_day: 0,
         url:'https://calendarific.com/api/v2/holidays?&api_key=',
+        full_url:'',
         holidays: [
             {
                 name: '',
@@ -26,7 +27,7 @@ let app = new Vue({
             {
                 name: '',
                 description: '',
-                date: ''
+                date: '',
             }
         ],
     },
@@ -36,31 +37,49 @@ let app = new Vue({
     methods:{
         async ushd(){
             try {
-                this.curr_year = parseInt(moment().format('YYYY'));
-                this.curr_month = parseInt(moment().format('M'));
-                this.curr_day = parseInt(moment().format('D'));
-                console.log(this.curr_year);
-                console.log(this.curr_month);
-                console.log(this.curr_day);
-                console.log(moment().add(-1,'days').format('D-M-YYYY'))
-                const the_url = this.url+this.key+this.country+this.year+this.month+this.day;
-                const response = await axios.get(the_url);
-                this.holidays = response.data.response.holidays;
-                this.addHolidaySequence(0,this.holidays[0].name,this.holidays[0].description,0);
-                //console.log(this.holidays.size);
-                //console.log(this.holidays[0]);
-                console.log(this.holidays[0].name);
-                console.log(this.holidays[0].description);
-                //this.most_important_hld = this.holidays[0];
+                this.changeDay(0);
+                const response1 = await axios.get(this.full_url);
+                this.holidays = response1.data.response.holidays;
+                this.addHolidaySequence(0,0);
+                
+                this.changeDay(1);
+                const response2 = await axios.get(this.full_url);
+                this.holidays = response2.data.response.holidays;
+                this.addHolidaySequence(1,1);
+                
+                this.changeDay(2);
+                const response3 = await axios.get(this.full_url);
+                this.holidays = response3.data.response.holidays;
+                this.addHolidaySequence(2,2);
+                
+                console.log(this.holiday_sequence);
             } catch (error) {
                 console.log(error);
             }
         },
-        addHolidaySequence(num, name, description, days) {
-            
-            this.holiday_sequence[num].date = moment().add(days,'days').format('MMMM Do YYYY');
-            this.holiday_sequence[num].name = name;
-            this.holiday_sequence[num].description = description;
+        changeDay(days){
+            this.curr_year = parseInt(moment().add(days,'days').format('YYYY'));
+            this.curr_month = parseInt(moment().add(days,'days').format('M'));
+            this.curr_day = parseInt(moment().add(days,'days').format('D'));
+            this.year = '&year='+this.curr_year.toString();
+            this.month = '&month='+this.curr_month.toString();
+            this.day = '&day='+this.curr_day.toString();
+            //this.day = '&day=25';
+            this.full_url= this.url+this.key+this.country+this.year+this.month+this.day;
+        },
+        addHolidaySequence(num, days) {
+            var the_name = 'Huh, nothing happens this day...';
+            var the_description = 'hmm.....';
+            //console.log(this.holidays.length);
+            if (this.holidays.length){
+                the_name = this.holidays[0].name;
+                the_description = this.holidays[0].description;
+            }
+            Vue.set(this.holiday_sequence, num, {
+                name: the_name,
+                description: the_description,
+                date: moment().add(days,'days').format('MMMM Do YYYY'),
+            });
         },
     },
 });
